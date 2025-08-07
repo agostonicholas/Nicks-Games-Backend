2ca981096f21f2eddd6a62701574c1b0a158dea1
agostonicholas <12q567@gmail.com>
2025-08-07 19:11:42 -0400

    fixed that fix

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const SCORE_FILE = path.join(__dirname, "scores.json");
 app.use(express.json());
 
 app.use(cors({
-    origin: "https://agostonicholas.github.io", "http://127.0.0.1:5500/"
+    origin: "https://agostonicholas.github.io", 
+    "http://127.0.0.1:5500"
 }));
 
 if (!fs.existsSync(SCORE_FILE)) {
